a4448268d0bb9431217ae2f06175c786f60806fe
LeonardoPigatti <leonardopigatti@yahoo.com.br>
2026-04-17 12:24:45 -0300

    primeiro commit

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+import Login from '../pages/Login.vue'
+import Dashboard from '../pages/Dashboard.vue'
+
+const routes: RouteRecordRaw[] = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/dashboard', component: Dashboard }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+// 🔐 proteção
+router.beforeEach((to, from, next) => {
+  const isAuth = localStorage.getItem('auth')
+
+  if (to.path === '/dashboard' && !isAuth) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
